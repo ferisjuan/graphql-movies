1693676b7e832cc8b601bf5e46a00ed6598043e8
Juan Feris <juan@Juans-MacBook-Pro.local>
2022-01-20 10:52:32 -0500

    feat(be): add root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,18 +1,27 @@
-import graphql, { GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
 
-const MovieType = new GraphQLQbjectType({
+const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
-    })
+    }),
 })
 
-const DirectorType = new GraphQLObjectType({
-    name: 'Director',
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
     fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-    })
+        movie: {
+            type: MovieType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // TODO: get data from db
+            },
+        },
+    }),
+})
+
+export default new GraphQLSchema({
+    query: RootQuery
 })
