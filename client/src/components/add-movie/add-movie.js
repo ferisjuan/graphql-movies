5789d543a191188ca9b34b43bafba07d745e2593
Juan Feris <juan@Juans-MacBook-Pro.local>
2022-01-20 16:48:45 -0500

    feat(iu): add movie mutation

--- client/src/components/add-movie/add-movie.js
+++ client/src/components/add-movie/add-movie.js
@@ -1,34 +1,61 @@
-import { useQuery } from '@apollo/client'
-import { GET_DIRECTORS_QUERY } from '../queries'
+import { useState } from 'react'
+
+import { useMutation, useQuery } from '@apollo/client'
+import { ADD_MOVIE_MUTATION, GET_DIRECTORS_QUERY } from '../queries'
 
 export const AddMovies = () => {
     const { data, error, loading } = useQuery(GET_DIRECTORS_QUERY)
+    const [addMovie] = useMutation(ADD_MOVIE_MUTATION)
+
+    const [name, setName] = useState('')
+    const [genre, setGenre] = useState('')
+    const [directorId, setDirectorId] = useState('')
 
     const renderDirectors = () => {
         if (loading) return <option disabled>Loading...</option>
 
         if (error) return <option disabled>Something went wrong :(</option>
 
-        return data?.directors.map(director =>  (
+        return data?.directors.map(director => (
             <option key={director.id} value={director.id}>
                 {director.name}
             </option>
-        )
-    )}
+        ))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        addMovie({ variables: { name, genre, directorId } })
+    }
 
     return (
-        <form id='add-movie'>
+        <form id='add-movie' onSubmit={handleSubmit}>
             <div>
                 <label htmlFor='movie-name'>Movie Name:</label>
-                <input id='movie-name' name='movie-name' type='text' />
+                <input
+                    id='movie-name'
+                    name='movie-name'
+                    onChange={e => setName(e.target.value)}
+                    type='text'
+                />
             </div>
             <div>
                 <label htmlFor='movie-genre'>Genre:</label>
-                <input id='movie-genre' name='movie-genre' type='text' />
+                <input
+                    id='movie-genre'
+                    name='movie-genre'
+                    onChange={e => setGenre(e.target.value)}
+                    type='text'
+                />
             </div>
             <div>
                 <label htmlFor='director'>Director</label>
-                <select name='director' id='director'>
+                <select
+                    name='director'
+                    id='director'
+                    onChange={e => setDirectorId(e.target.value)}
+                >
                     <option>Select a director</option>
                     {renderDirectors()}
                 </select>
--- client/src/components/queries.js
+++ client/src/components/queries.js
@@ -9,6 +9,15 @@ export const GET_DIRECTORS_QUERY = gql`
     }
 `
 
+export const ADD_MOVIE_MUTATION = gql`
+    mutation ($name: String!, $genre: String!, $directorId: ID!) {
+        addMovie(name: $name, genre: $genre, directorId: $directorId) {
+            id
+            name
+        }
+    }
+`
+
 export const GET_MOVIES_QUERY = gql`
     {
         movies {
