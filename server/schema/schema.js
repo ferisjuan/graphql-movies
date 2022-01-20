442e8b3cdeb86d3e3c523bc89a8750cf08dc8e71
Juan Feris <juan@Juans-MacBook-Pro.local>
2022-01-20 11:13:45 -0500

    feat(be): configure graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,12 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import _ from 'lodash'
+
+const movies = [
+    {name: 'Star Wars', director: 'George Lucas', id: '1'},
+    {name: 'Matrix', director: 'Wachowski', id: '2'},
+    {name: 'Interstellar', director: 'Nolan', id: '3'},
+    {name: 'Inception', director: 'Christopher Nolan', id: '4'},
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -17,6 +25,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
                 // TODO: get data from db
+                return _.find(movies, { id: args.id })
             },
         },
     }),
